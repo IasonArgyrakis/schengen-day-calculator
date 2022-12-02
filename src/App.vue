84ad2f0c5494165961aff050593552af1f949f1c
Iason Argyrakis <jasonargirakis@gmail.com>
2022-12-02 21:01:28 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/1599px-Flag_of_Europe.svg.png?20081021155534"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h2>Schengen Day Calculator</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/IasonArgyrakis"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Created By Iason Argyrakis</span>
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,130 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col
+
+      >
+        <v-card
+
+        >
+          <v-card-title>
+            How it works
+          </v-card-title>
+          <v-card-text>
+            Start by Entering the dates of your trip below
+
+
+          </v-card-text>
+          <v-container class="grey lighten-5">
+            <v-row>
+              <v-col md="4">
+                <v-date-picker
+                    v-model="newDateRange"
+                    range
+                    full-width
+                ></v-date-picker>
+                <v-btn class="mt-4" elevation="2" color="primary"  block @click="save">Add Dates</v-btn>
+
+              </v-col>
+              <v-col md="8">
+                <v-data-table
+                    :headers="headers"
+                    :items="daysInShengen"
+                    class="elevation-1 mt-3 pt-2"
+                >
+                  <template v-slot:[`item.actions`]="{ item }">
+                    <v-icon
+                        small
+                        @click="deleteItem(item)"
+                    >
+                      mdi-delete
+                    </v-icon>
+                  </template>
+                  <template v-slot:no-data>
+                    <p>No dates Added</p>
+                  </template>
+                </v-data-table>
+              </v-col>
+            </v-row>
+          </v-container>
+
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+
+  components: {},
+  data() {
+    return {
+
+      editedItem: {
+        start: undefined,
+        end: undefined,
+      },
+
+      headers: [
+        {
+          text: 'Entry Date',
+          align: 'start',
+          sortable: true,
+          value: 'startDate',
+        },
+        {
+          text: 'Exit Date',
+          sortable: false,
+          value: 'endDate',
+        },
+        {
+          text: '# of Days',
+          sortable: false,
+          value: 'count',
+        },
+        {
+          text: 'Actions',
+          sortable: false,
+          value: 'actions',
+        },
+
+      ],
+      newDateRange: [],
+      daysInShengen: [],
+
+    }
+  },
+  methods: {
+
+    editItem(item) {
+      this.editedIndex = this.days.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      this.daysInShengen.splice(item.index, 1)
+    },
+
+
+    save() {
+      this.newDateRange.sort()
+      this.daysInShengen.push(
+          {
+            startDate: new Date(this.newDateRange[0]),
+            endDate: new Date(this.newDateRange[1]),
+            count: ( new Date(this.newDateRange[1]).getTime()
+                    - new Date(this.newDateRange[0]).getTime()
+            ) / (1000 * 3600 * 24) } )
+      this.newDateRange = []
+      this.daysInShengen.sort();
+      this.$forceUpdate()
+    },
   }
+
 }
 </script>
